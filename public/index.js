daa82251d657498d74616702ceeeba7d27032839
chskela <chskela@mail.ru>
2019-05-22 19:25:31 +0300

    basic js

---
+++ public/index.js
@@ -0,0 +1,11 @@
+const keys = document.querySelectorAll('.key');
+const out = document.querySelector('.input');
+
+
+for (let i=0; i<keys.length; i++) {
+    keys[i].addEventListener('click', () => {
+        console.log(keys[i].textContent);
+        
+        out.innerHTML = keys[i].textContent;
+    })
+}
