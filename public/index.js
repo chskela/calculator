4ff23a640045dfc094026fc3eda823c3808cb920
chskela <chskela@mail.ru>
2019-05-28 16:22:51 +0300

    Clear

--- public/index.js
+++ public/index.js
@@ -4,23 +4,31 @@ const plus = document.querySelector('#plus');
 const minus = document.querySelector('#minus');
 const division = document.querySelector('#division');
 const multiplication = document.querySelector('#multiplication');
-
 const equal = document.querySelector('#equal');
+const root = document.querySelector('#root');
+const square =document.querySelector('#square');
+const clear = document.querySelector('#clear');
 
 
 let value = '';
 let memory = 0;
 let flag = 0;
+let result = 0;
+
+const chekResult = result => {
+  result = '' + result;
+  result = result.slice('.');
+  if (result[0].length <= 17) return result[0]+result[1].toFixed()
+}
 
 for (let i=0; i<digitals.length; i++) {
-    digitals[i].addEventListener('click', () => {
-        console.log(digitals[i].textContent);
-        if(out.textContent.length < 10){
-        value += digitals[i].textContent;
-        out.innerHTML = value;
-        }
-
-    })
+  digitals[i].addEventListener('click', () => {
+    if(out.textContent.length < 18){
+      value += digitals[i].textContent;
+      out.innerHTML = value;
+    }
+    
+  })
 }
 
 plus.addEventListener('click', () => {
@@ -48,24 +56,37 @@ division.addEventListener('click', () => {
   out.innerHTML = 0;
   flag = 4;
 });
+root.addEventListener('click', () => {
+  out.innerHTML = value = Math.pow(+value, 2)
+});
+square.addEventListener('click', () => {
+  out.innerHTML = value = Math.pow(+value, 0.5)
+});
+clear.addEventListener('click', () => {
+  memory = 0;
+  value = '';
+  result = 0;
+  out.innerHTML = 0;
+})
 
 equal.addEventListener('click', () => {
-  console.log(flag);
-  
   switch (flag) {
     case 1:
-      out.innerHTML = memory + +value;
+      result = memory + +value;
       break;
     case 2:
-      out.innerHTML = memory - +value;
+      result = memory - +value;
       break;
     case 3:
-      out.innerHTML = memory * +value;
+      result = memory * +value;
       break;
     case 4:
-      out.innerHTML = memory / +value;
+      result = memory / +value;
       break;
-
+    
   }
+  value = '';
+  out.innerHTML = result;
+  chekResult(result);
   
 })
